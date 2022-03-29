9a404029aea1f4186c932a86f1882453f0fbe26c
AvishekThapa1997 <avishekthapa1997@gmail.com>
2022-03-29 10:52:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./data.json";
+import "./App.css";
+import { FaFolderPlus, FaFolderMinus, FaFile } from "react-icons/fa";
+import { useState, useRef } from "react";
+import { Transition } from "react-transition-group";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((file) => {
+        return <Directory key={file.name} file={file} />;
+      })}
     </div>
   );
 }
 
+const Directory = ({ file }) => {
+  return file.type.toLowerCase() === "folder" ? (
+    <Folder name={file.name} files={file.files} />
+  ) : (
+    <File name={file.name} />
+  );
+};
+
+const Folder = ({ name, files }) => {
+  const [isExpand, setExpand] = useState(false);
+  const subFolderRef = useRef();
+  const expandHandler = (event) => {
+    setExpand((prevState) => !prevState);
+  };
+  const defaultStyle = {
+    paddingLeft: "2rem",
+    transformOrigin: "top",
+    transform: "scaleY(0)",
+    transition: "transform 0.3s",
+  };
+  const transitionStyles = {
+    entered: {
+      transform: "scaleY(1)",
+    },
+    entering: {
+      transform: "scaleY(0)",
+    },
+  };
+  return (
+    <div className="folder">
+      <p className="folder__name">
+        {isExpand ? (
+          <FaFolderMinus
+            onClick={expandHandler}
+            style={{ cursor: "pointer" }}
+          />
+        ) : (
+          <FaFolderPlus onClick={expandHandler} style={{ cursor: "pointer" }} />
+        )}
+        {name}
+      </p>
+      {files?.length ? (
+        <Transition
+          in={isExpand}
+          nodeRef={subFolderRef}
+          timeout={100}
+          appear={isExpand}
+          unmountOnExit={true}
+        >
+          {(state) => {
+            return (
+              <div
+                className="sub"
+                style={{ ...defaultStyle, ...transitionStyles[state] }}
+                ref={subFolderRef}
+              >
+                {files.map((file) => (
+                  <Directory key={file.name} file={file} />
+                ))}
+              </div>
+            );
+          }}
+        </Transition>
+      ) : null}
+    </div>
+  );
+};
+
+const File = ({ name }) => {
+  return (
+    <p className="file__name">
+      <FaFile />
+      {name}
+    </p>
+  );
+};
 export default App;
